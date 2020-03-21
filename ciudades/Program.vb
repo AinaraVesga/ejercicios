4b74f5215f97529f9d5088692144ff80cfa7de81
Ainara <ikawv@plaiaundi.net>
2020-03-21 16:41:42 +0100

    Proyecto ciudades: funcion separar v1

--- ciudades/Program.vb
+++ ciudades/Program.vb
@@ -4,6 +4,8 @@ Module Program
     Private i As Integer
     Private j As Integer
 
+
+    ' Funcion para saber si dos palabra están compuestas por las mismas letras
     Public Function mismasLetras(palabra1 As String, palabra2 As String)
 
         If (palabra1.Length <> palabra2.Length) Then
@@ -26,7 +28,7 @@ Module Program
                         j += 1
                     Else
                         palabra2 = Replace(palabra2, letra2, "", 1, 1)
-                        Console.WriteLine("PALABRA1: " + palabra1 + " PALABRA2: " + palabra2)
+                        ' Console.WriteLine("PALABRA1: " + palabra1 + " PALABRA2: " + palabra2)
                         esta = True
                     End If
                 End While
@@ -46,8 +48,49 @@ Module Program
         End If
     End Function
 
+    Public Function separar(ciudades As String())
+
+        Dim iguales As Boolean
+        Dim multiLista As New List(Of List(Of String))
+        Dim lista As New List(Of String)
+
+        lista.Add(ciudades(0))
+        multiLista.Add(lista)
+
+        Console.WriteLine(multiLista(0)(0))
+
+        For i As Integer = 1 To ciudades.Length
+            Dim j = 0
+            Dim salir = False
+            While j < multiLista.Count And salir = False
+                iguales = mismasLetras(ciudades(i), multiLista(j)(0))
+                If iguales = True Then
+                    multiLista(j).Add(ciudades(i))
+                    salir = True
+                Else
+                    j += 1
+                End If
+
+            End While
+
+            If salir = False Then
+                multiLista.Add(New List(Of String) From {ciudades(i)})
+            End If
+
+        Next
+
+        Return multiLista
+
+    End Function
+
     Public Sub Main(args As String())
-        Dim iguales = mismasLetras("tokyo", "kyoto")
-        Console.WriteLine(iguales)
+        ' Dim iguales = mismasLetras("tokyo", "kyoto")
+
+        Dim ciudades = New String() {"tokyo", "london", "rome", "donlon", "kyoto", "paris"}
+
+        Dim res = separar(ciudades)
+
+        For i As Integer = 0 To res.
+
     End Sub
 End Module
