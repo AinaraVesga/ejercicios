6660eaeaabb52ccec038b1284ea4640949b3f46e
Ainara <ikawv@plaiaundi.net>
2020-03-22 12:42:21 +0100

    Proyecto numPrimos: terminado

--- numPrimos/Program.vb
+++ numPrimos/Program.vb
@@ -7,7 +7,7 @@ Module Program
         Dim primo = True
 
         While i > 1 And primo = True
-            Console.WriteLine(i)
+            ' Console.WriteLine(i)
             If x Mod i = 0 Then
                 primo = False
             Else
@@ -16,10 +16,33 @@ Module Program
         End While
 
         Return primo
+
+    End Function
+
+    Public Function primos(num As Integer)
+
+        Dim count = 0
+
+        For i As Integer = 1 To num
+            If esPrimo(i) Then
+                count += 1
+            End If
+        Next
+
+        Return count
+
     End Function
 
     Public Sub Main(args As String())
-        Dim x = 8
-        Console.WriteLine(esPrimo(x))
+
+        Dim num As Integer
+
+        Console.WriteLine("Introduzca un número: ")
+        num = Console.ReadLine()
+
+        Dim count = primos(num)
+        Console.WriteLine("Hay " + CStr(count) + " números primos desde 1 hasta " + CStr(num) + ".")
+
+
     End Sub
 End Module
