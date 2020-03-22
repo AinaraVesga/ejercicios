39d4d293104e247463279f714bd2db3e083efd71
Ainara <ikawv@plaiaundi.net>
2020-03-22 12:18:16 +0100

    Proyecto fibonnacci: terminado

--- fibonacci/Program.vb
+++ fibonacci/Program.vb
@@ -1,7 +1,29 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+
+    Public Function fibonacci(n As Integer)
+        If n = 1 Then
+            Return 1
+        ElseIf n = 2 Then
+            Return 1
+        Else
+            Return fibonacci(n - 1) + fibonacci(n - 2)
+        End If
+    End Function
+
+    Public Sub Main(args As String())
+
+        Dim n = 30
+        Dim res As Integer
+        Dim resul = ""
+
+        For i As Integer = n To 1 Step -1
+            res = fibonacci(i)
+            resul = CStr(res) + " - " + resul
+        Next
+
+        Console.WriteLine(resul)
+
     End Sub
 End Module
